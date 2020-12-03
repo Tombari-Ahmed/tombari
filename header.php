6cfb9d7d8ca37a684e1c5cd74a77f0edad659cef
Tombari-Ahmed <61208883+Tombari-Ahmed@users.noreply.github.com>
2020-12-03 13:52:44 +0100

    Update header.php

--- header.php
+++ header.php
@@ -1,3 +1,5 @@
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
 <?php session_start(); ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -29,7 +31,7 @@
                 else
                 {
                     echo '  <li class="nav-item"><a href="logindesign.php" class="btn btn-outline-light">Login</a></li>
-                    <li class="nav-item"><a href="signupdesign.php" class="btn btn-outline-light ml-3">Sign Up</a></li>';
+                    <li class="nav-item"><a href="signup.php" class="btn btn-outline-light ml-3">Sign Up</a></li>';
                 }
 
                 ?>
