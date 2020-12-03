47b0c04c6c5389858b8fb112d511919255a0e6ff
Tombari-Ahmed <61208883+Tombari-Ahmed@users.noreply.github.com>
2020-12-03 13:51:41 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
 <?php require_once('header.php'); ?>
     <h4 class="display-4 text-center mt-5">ISIMED LOGIN</h4>
 <?php require_once('footer.php'); ?>
